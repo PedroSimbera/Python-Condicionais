93ac8569c2935ef098ad0047ac019b287f5b6263
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-04 20:52:57 -0300

    sobre if e else

---
+++ IfElse.py
@@ -0,0 +1,7 @@
+idade = int(input("Qual sua idade?"))
+
+if idade > 18:
+    print("Maior de idade")
+
+#Necessita de indentação para reconhecer o conteúdo dentro do if
+#No Python, diferente do Java e C# utiliza ":" ao invés de "{}"
