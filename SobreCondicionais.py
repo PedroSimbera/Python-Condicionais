1cbe8526702a78ab8079be57ad686bdc0c64570f
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-04 15:12:19 -0300

    sobre o que é condicionais

---
+++ SobreCondicionais.py
@@ -0,0 +1,14 @@
+# Condicionais: Algo que precisa ser atendido para que uma ação seja feita.
+#Ex: Se chover 
+# Saio com guarda-chuva
+# Senão
+# Não saiu com guarda-chuva
+
+# Se é o IF na programação
+# Sendo a condição verdadeira, a ação logo abaixo será feita, caso seja falsa, a outra ação será feita, se tiver o else
+
+# Pode fazer mais de uma proposição, com o uso dos operadores And Or Not && || !
+
+# If/else
+# Switch case - no python é Match
+#Ternário
