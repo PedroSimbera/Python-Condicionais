ec033972f2967c9bff2688396dce90a1980bd7f9
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-04 21:56:19 -0300

    sobre match case

---
+++ SwitchCase - Match.py
@@ -0,0 +1,10 @@
+# Switch Case - para o Python há o Match Case
+
+a = "Pedro"
+match a:
+    case"Antonio":
+        print("Não é Pedro")
+    case"Julio":
+        print("Não é Pedro")
+    case"Jose":
+        print("Não é Pedro")
