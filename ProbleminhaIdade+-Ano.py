0e61d8dc45ffcde0c02fb110898d86696ca54922
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-05 19:57:26 -0300

    sobre datetime e ano nascimento, descobrindo idade atualizaçao

--- ProbleminhaIdade+-Ano.py
+++ ProbleminhaIdade+-Ano.py
@@ -2,7 +2,7 @@ import datetime
 anoNascimento = int(input("Qual seu ano de nascimento: "))
 anoAtual = datetime.date.today().year
 idade = anoAtual - anoNascimento
-if (idade>18):
+if (idade>=18):
     print("Maior de idade, você tem", idade)
 else:
     print("Menor de idade, você tem", idade)
