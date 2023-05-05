2bde59fe0fac1a984193ada3f9f97c4f05f56114
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-04 21:51:16 -0300

    sobre ternária

---
+++ Ternaria.py
@@ -0,0 +1,5 @@
+#Meio para que apenas com uma linha de código possa realizar a condição if e else
+
+
+idade = int(input("Digite a idade: "))
+print("Maior de idade") if idade >=18 else print("Menor de idade")
